2a7070ed6b91d8b0f0b66a63286ed27113980429
Zero Ash <admin@remiliascarlet.com>
2026-01-09 03:51:54 +0800

    feat: add 404 page for handle SPA route

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -1,9 +1,18 @@
+import { resolve } from "path";
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        notFound: resolve(__dirname, "404.html"),
+      },
+    },
+  },
 });
